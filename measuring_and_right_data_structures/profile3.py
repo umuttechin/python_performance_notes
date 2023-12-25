27bd1b396d98911d6c2dc3283ad0254cde430fe2
UmutTEKİN <umuttechin@gmail.com>
2023-12-25 18:30:50 +0100

    Update profile3.py

--- measuring_and_right_data_structures/profile3.py
+++ measuring_and_right_data_structures/profile3.py
@@ -1,3 +1,25 @@
+"""
+pip install memory_profiler
+
+ $ python -m memory_profiler profile3.py
+Filename: profile3.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+     1   18.828 MiB   18.828 MiB           1   @profile
+     2                                         def create_big_list():
+     3   95.141 MiB   76.312 MiB           1       return 10_000_000 * [0]
+
+
+Filename: profile3.py
+
+Line #    Mem usage    Increment  Occurrences   Line Contents
+=============================================================
+     6   95.141 MiB   95.141 MiB           1   @profile
+     7                                         def create_huge_list():
+     8  324.031 MiB  228.891 MiB           1       return 30_000_000 * [0]
+
+"""
 @profile
 def create_big_list():
     return 10_000_000 * [0]
