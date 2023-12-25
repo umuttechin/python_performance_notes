f433c7e26f065bca9acccd00120c73f798b3390a
UmutTEKİN <umuttechin@gmail.com>
2023-12-25 19:08:24 +0100

    Update faster_functions.py

--- optimizing/faster_functions.py
+++ optimizing/faster_functions.py
@@ -10,6 +10,26 @@ Calling other functions
     More reusable
     Easier to maintain
     Slower performance(overhead of calling many times)
+
+------------------------------------------------------------------
+kernprof -lv faster_functions.py
+Wrote profile results to faster_functions.py.lprof
+Timer unit: 1e-06 s
+
+Total time: 0.00013 s
+File: faster_functions.py
+Function: main at line 21
+
+Line #      Hits         Time  Per Hit   % Time  Line Contents
+==============================================================
+    21                                           @profile
+    22                                           def main():
+    23         1          1.0      1.0      0.8      SIZE = 25
+    24         1         36.0     36.0     27.7      [random.randint(1, 100) for _ in range(SIZE)]
+    25         1         30.0     30.0     23.1      [get_random() for _ in range(SIZE)]
+    26         1         31.0     31.0     23.8      [(lambda: random.randint(0, 100))() for _ in range(SIZE)]
+    27         1         32.0     32.0     24.6      [(lambda: get_random())() for _ in range(SIZE)]
+
 """
 
 
