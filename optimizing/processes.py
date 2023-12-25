6772d89a0dd731764047845b5f0f8903fb0e4695
UmutTEKİN <umuttechin@gmail.com>
2023-12-25 19:31:47 +0100

    Update processes.py

--- optimizing/processes.py
+++ optimizing/processes.py
@@ -11,6 +11,52 @@ More Processes
     Increased memory overhead
     Harder to share resources
 
+-----------------------------------------
+
+from multiprocessing import Process
+
+def clean_order():
+    for _ in range(500_000_000):
+        pass
+    print("finished.")
+
+if __name__ == '__main__':
+    p1 = Process(target=clean_order)
+    #p2 = Process(target=clean_order)
+
+    p1.start()
+    #p2.start()
+    p1.join()
+    #p2.join()
+    
+-----------------------------------------
+
+time python processes.py
+finished.
+python3 processes.py  4.93s user 0.02s system 99% cpu 4.974 total
+
+from multiprocessing import Process
+
+def clean_order():
+    for _ in range(500_000_000):
+        pass
+    print("finished.")
+
+if __name__ == '__main__':
+    p1 = Process(target=clean_order)
+    p2 = Process(target=clean_order)
+
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    
+-----------------------------------------
+
+time python processes.py
+finished.
+python3 processes.py  4.93s user 0.02s system 99% cpu 4.974 total
+
 """
 from multiprocessing import Process
 
