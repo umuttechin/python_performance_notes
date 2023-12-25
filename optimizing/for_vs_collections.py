bda5043522b5fc8fcbb00e8d3b77312d17514924
UmutTEKİN <umuttechin@gmail.com>
2023-12-25 18:58:12 +0100

    Update for_vs_collections.py

--- optimizing/for_vs_collections.py
+++ optimizing/for_vs_collections.py
@@ -10,6 +10,26 @@ List comprehension are
     concise
     faster for simple logic
     set and dictionary comprehensions
+
+----------------------------
+
+kernprof -lv for_vs_collections.py
+Wrote profile results to for_vs_collections.py.lprof
+Timer unit: 1e-06 s
+
+Total time: 0.114319 s
+File: for_vs_collections.py
+Function: main at line 28
+
+Line #      Hits         Time  Per Hit   % Time  Line Contents
+==============================================================
+    28                                           @profile
+    29                                           def main():
+    30         1      98962.0  98962.0     86.6      orders = [random.randint(1, 100) for _ in range(100000)]
+    31                                           
+    32         1       9926.0   9926.0      8.7      loop(orders)
+    33         1       5431.0   5431.0      4.8      comprehension(orders)
+    
 """
 import random
 
@@ -33,4 +53,3 @@ def main():
     comprehension(orders)
 
 main()
-
