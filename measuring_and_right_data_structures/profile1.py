6553bde29054629a01ac2805c14e0e6e2413646e
UmutTEKİN <umuttechin@gmail.com>
2023-12-25 18:27:28 +0100

    Update profile1.py

--- measuring_and_right_data_structures/profile1.py
+++ measuring_and_right_data_structures/profile1.py
@@ -19,6 +19,22 @@ python -m profile profile1.py
    100000    0.043    0.000    0.043    0.000 profile1.py:7(do_stuff)
         1    0.000    0.000    0.098    0.098 profile:0(<code object <module> at 0x104c0e710, file "profile1.py", line 1>)
         0    0.000             0.000          profile:0(profiler)
+------------------------------
+python -m cProfile profile1.py                                                                                                                                                                                         
+0.009616851806640625 100007 function calls in 0.010 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.010    0.010 profile1.py:1(<module>)
+        1    0.007    0.007    0.010    0.010 profile1.py:3(heavy_work)
+   100000    0.003    0.000    0.003    0.000 profile1.py:7(do_stuff)
+        1    0.000    0.000    0.010    0.010 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        2    0.000    0.000    0.000    0.000 {built-in method time.time}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+      
 """
 import time
 
